1f70c2e517082c23580e9ab534c108f540c6c6d9
fiona-spencer <fionagspencer@gmail.com>
2024-03-09 22:54:24 -0500

    created and initialized dataset_generator.js

---
+++ node/dataset_generator.js
@@ -0,0 +1,8 @@
+const constants={};
+
+constants.DATA_DIR="../data";
+constants.RAW_DIR=constants.DATA_DIR + "/raw";
+constants.DATASET_DIR=constants.DATA_DIR + "/dataset";
+constants.JSON_DIR=constants.DATA_SET = "/json";
+constants.IMG_DIR=constants.DATASET_DIR+"/img";
+constants.SAMPLES=constants.DATASET_DIR+"/samples.json";
